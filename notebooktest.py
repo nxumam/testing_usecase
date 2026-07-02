2c02e20cb46b8f286d412d2bd4d08ce51fb236f8
nxumam <86078310+nxumam@users.noreply.github.com>
2026-07-02 12:27:41 +0200

    Update notebooktest.py

--- notebooktest.py
+++ notebooktest.py
@@ -1,1 +1,5 @@
-print("Testing")
+import sys
+sys.path.append("/databricks/driver/testing_usecase")  # or wherever it cloned
+
+from mypackage import greet, add
+greet("Databricks")   # 'Hello, Databricks!'
